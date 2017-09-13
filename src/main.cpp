557910676e593717570189e6ce171214cf64ddc7
enindza <zx81one@gmail.com>
2017-09-14 00:08:36 +0200

    Authorisation added.

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ void setup() {
   // ArduinoOTA.setHostname("myesp8266");
 
   // No authentication by default
-  // ArduinoOTA.setPassword("admin");
+  ArduinoOTA.setPassword("admin");
 
   // Password can be set with it's md5 value as well
   // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
